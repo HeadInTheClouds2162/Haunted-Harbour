2bcc84a27ca8ed48dd7149d9477000d64c9d8f80
HeadInTheClouds2162 <jenny.chen.2031@utschools.ca>
2026-02-06 18:42:48 -0500

    :D
    :D

--- Assets/Scripts/Characters/Player/WeaponController.cs
+++ Assets/Scripts/Characters/Player/WeaponController.cs
@@ -5,10 +5,11 @@ using UnityEngine.InputSystem;
 public class WeaponController : MonoBehaviour, IInputReceiver
 {
     [SerializeField] private Weapon currentWeapon;
-
+    [SerializeField] private Transform Hand;
     public void BindControls(PlayerInput reference)
     {
         reference.actions["Shoot"].performed += Shoot;
+        reference.actions["MousePosition"].performed += PointHandTarget;
     }
 
     private void Shoot(InputAction.CallbackContext obj)
@@ -26,7 +27,22 @@ public class WeaponController : MonoBehaviour, IInputReceiver
     public void UnbindControls(PlayerInput reference)
     {
         reference.actions["Shoot"].performed -= Shoot;
+        reference.actions["MousePosition"].performed -= PointHandTarget;
     }
+
+    private void PointHandTarget(InputAction.CallbackContext obj)
+    {
+        Vector2 mousePosition = obj.ReadValue<Vector2>();
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        float rise =  mousePosition.y - Hand.position.y;
+        float run = mousePosition.x - Hand.position.x;
+        float angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+        Hand.rotation = Quaternion.Euler(0, 0, angle);
+    }
+    
+    
+    
+    
     
     
         
